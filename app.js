dbde7a46a0f6043a593bd5513ed9541dbe57d101
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-09-19 08:03:58 +0800

    2-6 洋葱模型

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const Koa=require("Koa");
+const Koa = require("Koa");
 
 //实例化应用程序对象
-const app=new Koa();
+const app = new Koa();
 
 //应用中间件
 
 // 发送HTTP KOA  接受HTTP
 
 //函数
-function test() {
-    console.log('hello', 'ReginYuan');
-}
+// function test() {
+//     console.log('hello', 'ReginYuan');
+// }
 
-//将函数注册成中间件 
-app.use(test);
+//将函数注册成中间件
+// app.use(test);
+
+app.use(async(ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(2);
+});
+
+app.use(async(ctx, next) => {
+    console.log(3);
+    await next();
+    console.log(4);
+});
 
 // 设置监听端口
 app.listen(3000);
