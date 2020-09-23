03316370d591d96addfe4d92a1dd29fae9fce669
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-09-23 16:27:24 +0800

    3-6 requireDirectory实现路由自动

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // Koa组件
 const Koa = require("Koa");
-const book = require('./api/v1/boock');
-const classic = require('./api/v1/classic');
+// require-directory  实现路由组件自动化插件
+const requireDirectory = require('require-directory');
+
+
 
 //实例化应用程序对象
 const app = new Koa();
+//路由自动化 设置路由的路径并调用注册中间件的方法
+const modules = requireDirectory(module, './api', {
+    visit: whenLoadModule
+});
 
+// 注册中中间件
+function whenLoadModule(obj) {
+    if (obj instanceof Router) {
+        app.use(obj.routes());
+    }
+
+}
 
 
 // 注册路由中间件
